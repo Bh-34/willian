4f63432802f3a1c15517e4102786954c3abc89ad
vitorlange2 <vitorlange953@gmail.com>
2026-02-02 19:48:15 -0300

    ola

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '@/views/Dashboard.vue'
-import Produtos from '@/views/produtos.vue'
+import Produtos from '@/views/Produtos.vue'
 import Login from '@/views/login.vue'
 import Cadastro from '@/views/cadastro.vue'
-import Perfil from '@/views/perfil.vue'
+import Perfil from '@/views/Perfil.vue'
 import { isAuthenticated } from '@/services/authService'
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Dashboard },
   { path: '/produtos', name: 'Produtos', component: Produtos, meta: { requiresAuth: true } },
-  { path: '/login', name: 'login', component: Login },
+  { path: '/login', name: 'Login', component: Login },
   { path: '/cadastro', name: 'cadastro', component: Cadastro },
   {path: '/perfil', name: 'perfil', component: Perfil, meta: { requiresAuth: true } }
 ]
